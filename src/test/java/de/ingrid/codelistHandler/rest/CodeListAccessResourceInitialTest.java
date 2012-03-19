a47c09e4b0562d2a50490a1ee5eeeee45ac26371
aw <aw@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2012-03-19 11:15:47 +0000

    - added tests for getting codelists including correct sorting

--- src/test/java/de/ingrid/codelistHandler/rest/CodeListAccessResourceInitialTest.java
+++ src/test/java/de/ingrid/codelistHandler/rest/CodeListAccessResourceInitialTest.java
@@ -1,19 +1,136 @@
 package de.ingrid.codelistHandler.rest;
 
+import static org.junit.Assert.*;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import de.ingrid.codelistHandler.CodeListManager;
+import de.ingrid.codelists.util.CodeListUtils;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/application-context-rest-test.xml"})
 public class CodeListAccessResourceInitialTest {
 
+    @Autowired
+    private CodeListManager manager;
+    
     @Before
     public void setUp() throws Exception {
-    }
-    
-    @Test
-    public final void testGetCodeLists() {
-        //List<CodeList> cls = CodeListManager.getInstance().getCodeLists();
-        //assertTrue(cls.isEmpty());
+//        CodeListService service = new CodeListService();
+//        //service.setComm(comm)
+//        List<ICodeListPersistency> persistencies = new ArrayList<ICodeListPersistency>();
+//        XmlCodeListPersistency p = new XmlCodeListPersistency();
+//        p.setPathToXml("/src/test/resources/codelists-test.xml");
+//        persistencies.add(p);
+//        service.setPersistencies(persistencies);
+//        
+//        cm = new CodeListManager();
+//        cm.setCodeListService(service);
         
     }
     
+    @Test
+    public final void testGetCodeListsSortIdDec() throws JSONException {
+        JSONArray list = new JSONArray((String)manager.getCodeListsAsJson("id", null, CodeListUtils.SORT_DECREMENT));
+        assertTrue(list.length() > 0);
+        assertTrue(((JSONObject)list.get(0)).get("id").equals("99999999"));
+        assertTrue(((JSONObject)list.get(list.length()-1)).get("id").equals("100"));
+        
+        Long previousId = 99999999L;
+        for (int i=0; i<list.length(); i++) {
+            JSONObject o = (JSONObject) list.get(i);
+            assertTrue(previousId >= o.getLong("id"));
+            previousId = o.getLong("id");
+        }
+    }
+    
+    @Test
+    public final void testGetCodeListsSortIdInc() throws JSONException {
+        JSONArray list = new JSONArray((String)manager.getCodeListsAsJson("id", null, CodeListUtils.SORT_INCREMENT));
+        assertTrue(list.length() > 0);
+        assertTrue(((JSONObject)list.get(0)).get("id").equals("100"));
+        assertTrue(((JSONObject)list.get(list.length()-1)).get("id").equals("99999999"));
+        
+        Long previousId = 100L;
+        for (int i=0; i<list.length(); i++) {
+            JSONObject o = (JSONObject) list.get(i);
+            assertTrue(previousId <= o.getLong("id"));
+            previousId = o.getLong("id");
+        }
+    }
+    
+    @Test
+    public final void testGetCodeListsSortNameDec() throws JSONException {
+        JSONArray list = new JSONArray((String)manager.getCodeListsAsJson("name", null, CodeListUtils.SORT_DECREMENT));
+        assertTrue(list.length() > 0);
+        assertTrue(((JSONObject)list.get(0)).get("name").equals("Zeitbezug des Datensatzes - Typ"));
+        assertTrue(((JSONObject)list.get(list.length()-1)).has("name") == false);
+        
+        String previousName = "Zeitbezug des Datensatzes - Typ";
+        for (int i=0; i<list.length(); i++) {
+            JSONObject o = (JSONObject) list.get(i);
+            if (o.has("name")) {
+                if (previousName == null) {
+                    assertTrue("Wrong String sorting! NULL value inbetween valid values!", false);
+                }
+                assertTrue(previousName.compareTo(o.getString("name")) >= 0);
+                previousName = o.getString("name");
+            } else {
+                previousName = null;
+            }
+        }
+    }
+    
+    @Test
+    public final void testGetCodeListsSortNameInc() throws JSONException {
+        JSONArray list = new JSONArray((String)manager.getCodeListsAsJson("name", null, CodeListUtils.SORT_INCREMENT));
+        assertTrue(list.length() > 0);
+        assertTrue(((JSONObject)list.get(0)).has("name") == false);
+        assertTrue(((JSONObject)list.get(list.length()-1)).get("name").equals("Zeitbezug des Datensatzes - Typ"));
+        
+        
+        String previousName = null;
+        for (int i=0; i<list.length(); i++) {
+            JSONObject o = (JSONObject) list.get(i);
+            if (o.has("name")) {
+                if (previousName == null) {
+                    previousName = o.getString("name");
+                } else {
+                    assertTrue(previousName.compareTo(o.getString("name")) <= 0);
+                    previousName = o.getString("name");
+                }
+            } else {
+                if (previousName != null) {
+                  assertTrue("Wrong String sorting! NULL value inbetween valid values!", false);
+                }
+                previousName = null;
+            }
+        }
+    }
+ 
+    @Test
+    public final void testGetCodeListsLastModifiedNow() throws JSONException {
+        JSONArray list = new JSONArray((String)manager.getCodeListsAsJson("name", String.valueOf(System.currentTimeMillis()), CodeListUtils.SORT_INCREMENT));
+        assertTrue(list.length() == 0);
+    }
+    
+    @Test
+    public final void testGetCodeListsLastModifiedEarlier() throws JSONException {
+        JSONArray list = new JSONArray((String)manager.getCodeListsAsJson("name", "1331741000000", CodeListUtils.SORT_INCREMENT));
+        assertTrue(list.length() == 1);
+        JSONObject o = (JSONObject) list.get(0);
+        assertTrue("1000".equals(o.getString("id")));
+    }
+    
+    public void setManager(CodeListManager manager) {
+        this.manager = manager;
+    }
 }
