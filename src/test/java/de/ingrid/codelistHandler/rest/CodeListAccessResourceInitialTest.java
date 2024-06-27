484aa307780d9c318343b43dac76c061b3bff351
André Wallat <andre.wallat@wemove.com>
2024-06-27 21:28:35 +0200

    Set codelists to be ignored from any updates (#6430)

--- src/test/java/de/ingrid/codelistHandler/rest/CodeListAccessResourceInitialTest.java
+++ src/test/java/de/ingrid/codelistHandler/rest/CodeListAccessResourceInitialTest.java
@@ -7,12 +7,12 @@
  * Licensed under the EUPL, Version 1.2 or – as soon they will be
  * approved by the European Commission - subsequent versions of the
  * EUPL (the "Licence");
- * 
+ *
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * https://joinup.ec.europa.eu/software/page/eupl
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,6 +34,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -53,7 +54,7 @@ public class CodeListAccessResourceInitialTest {
         XmlCodeListPersistency xmlCodeListPersistency = new XmlCodeListPersistency();
         xmlCodeListPersistency.setPathToXml("src/test/resources/codelists");
         codeListService.setPersistencies(List.of(xmlCodeListPersistency));
-        manager = new CodeListManager(codeListService, new Migrator());
+        manager = new CodeListManager(codeListService, new Migrator(), new ArrayList<>());
     }
 
     @BeforeAll
