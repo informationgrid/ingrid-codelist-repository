e33f12a31b701c1eab79e25d8d50d12b7d04c34f
André Wallat <andre.wallat@wemove.com>
2026-09-11 12:50:53 +0200

    Update Spring Boot to 4.1.1 (#8697)

--- src/main/java/de/ingrid/codelistHandler/importer/bawOrderInfo/BawOrderInfoImporter.java
+++ src/main/java/de/ingrid/codelistHandler/importer/bawOrderInfo/BawOrderInfoImporter.java
@@ -7,12 +7,12 @@
  * Licensed under the EUPL, Version 1.2 or – as soon they will be
  * approved by the European Commission - subsequent versions of the
  * EUPL (the "Licence");
- * 
+ *
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * https://joinup.ec.europa.eu/software/page/eupl
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,7 +22,7 @@
  */
 package de.ingrid.codelistHandler.importer.bawOrderInfo;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import de.ingrid.codelistHandler.importer.Importer;
 import de.ingrid.codelists.CodeListService;
 import de.ingrid.codelists.model.CodeList;
@@ -84,7 +84,7 @@ public class BawOrderInfoImporter implements Importer {
         ObjectMapper objectMapper = new ObjectMapper();
         OrderInfoCodelistModel orderInfoCodelist;
         try {
-            orderInfoCodelist = objectMapper.readValue(url, OrderInfoCodelistModel.class);
+            orderInfoCodelist = objectMapper.readValue(url.openStream(), OrderInfoCodelistModel.class);
         } catch (Exception e) {
             log.error("Error parsing JSON data from " + url, e);
             throw e;
--- src/main/java/de/ingrid/codelistHandler/importer/inspireRegistry/InspireRegistryUtil.java
+++ src/main/java/de/ingrid/codelistHandler/importer/inspireRegistry/InspireRegistryUtil.java
@@ -7,12 +7,12 @@
  * Licensed under the EUPL, Version 1.2 or – as soon they will be
  * approved by the European Commission - subsequent versions of the
  * EUPL (the "Licence");
- * 
+ *
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * https://joinup.ec.europa.eu/software/page/eupl
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,7 +22,7 @@
  */
 package de.ingrid.codelistHandler.importer.inspireRegistry;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import de.ingrid.codelistHandler.importer.inspireRegistry.model.Item;
 import de.ingrid.codelistHandler.importer.inspireRegistry.model.InspireCodelistModel;
 import de.ingrid.codelists.model.CodeList;
@@ -45,13 +45,13 @@ public class InspireRegistryUtil {
         InspireCodelistModel inspireCodelist;
         InspireCodelistModel inspireCodelistEn;
         try {
-            inspireCodelist = objectMapper.readValue(urlGerman, InspireCodelistModel.class);
+            inspireCodelist = objectMapper.readValue(urlGerman.openStream(), InspireCodelistModel.class);
         } catch (Exception e) {
             log.error("Error parsing JSON data from " + urlGerman, e);
             throw e;
         }
         try {
-            inspireCodelistEn = objectMapper.readValue(urlEnglish, InspireCodelistModel.class);
+            inspireCodelistEn = objectMapper.readValue(urlEnglish.openStream(), InspireCodelistModel.class);
         } catch (Exception e) {
             log.error("Error parsing JSON data from " + urlEnglish, e);
             throw e;
